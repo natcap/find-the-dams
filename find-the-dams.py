58c48267e44f62c20ff4efb14ea73c9ff2fe68b3
Rich Sharp <richpsharp@gmail.com>
2019-07-30 17:08:29 -0700

    making root app

--- find-the-dams.py
+++ find-the-dams.py
@@ -1,10 +1,42 @@
 # coding=UTF-8
 """Module to process remote dam detection imagery."""
+import os
+import sys
+import logging
+
+from flask import Flask
+import flask
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=(
+        '%(asctime)s (%(relativeCreated)d) %(levelname)s %(name)s'
+        ' [%(funcName)s:%(lineno)d] %(message)s'),
+    stream=sys.stdout)
+logging.getLogger('taskgraph').setLevel(logging.INFO)
+
+LOGGER = logging.getLogger(__name__)
+
+APP = Flask(__name__, static_url_path='', static_folder='')
+APP.config['SECRET_KEY'] = b'\xe2\xa9\xd2\x82\xd5r\xef\xdb\xffK\x97\xcfM\xa2WH'
+
+
+@APP.route('/favicon.ico')
+def favicon():
+    return flask.send_from_directory(
+        os.path.join(APP.root_path, 'images'), 'favicon.ico',
+        mimetype='image/vnd.microsoft.icon')
+
+
+@APP.route('/')
+def index():
+    """Flask entry point."""
+    return 'hi'
 
 
 def main():
     """Entry point."""
-    pass
+    APP.run(host='0.0.0.0', port=8888)
 
 
 if __name__ == '__main__':
