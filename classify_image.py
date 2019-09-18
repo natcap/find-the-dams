d6d840b32db66bbc7189e0cf8e9fd57928e66e05
Rich Sharp <richpsharp@gmail.com>
2019-09-18 11:46:08 -0700

    adding flask framework to read and process a quad

--- classify_image.py
+++ classify_image.py
@@ -3,13 +3,19 @@ import sys
 import logging
 
 import PIL
+import PIL.ImageDraw
 import tensorflow as tf
 import numpy
 
-
-
 MODEL_PATH = 'models/fasterRCNN_08-26-withnotadams.pb'
+#MODEL_PATH = 'models/fasterRCNN_07-27_newimagery.pb'
 IMAGE_PATH = 'test_data/11133.png'
+#IMAGE_PATH = 'training_data/1005.png'
+#IMAGE_PATH = 'big_image.png'
+#IMAGE_PATH = 'test_a.png'
+#IMAGE_PATH = 'test_b.png'
+#IMAGE_PATH = 'small_test.png'
+#IMAGE_PATH = 'so_small.png'
 LOGGING_LEVEL = logging.DEBUG
 
 logging.basicConfig(
@@ -55,6 +61,7 @@ def do_detection(detection_graph, image_path):
 
     """
     image = PIL.Image.open(image_path).convert("RGB")
+    image.show()
     image_array = numpy.array(image)
 
     with detection_graph.as_default():
@@ -66,12 +73,25 @@ def do_detection(detection_graph, image_path):
             clss = detection_graph.get_tensor_by_name('detection_classes:0')
             num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
-            LOGGER.debug((image_array_expanded.shape, image_tensor, box, score, clss, num_detections))
-            # Actual detection
-            (box, score, clss, num_detections) = sess.run(
+            (box_list, score_list, _, _) = sess.run(
                     [box, score, clss, num_detections],
                     feed_dict={image_tensor: image_array_expanded})
-            LOGGER.debug((box, score, clss, num_detections))
+
+            # draw a bounding box
+            image_draw = PIL.ImageDraw.Draw(image)
+            for box, score in zip(box_list[0], score_list[0]):
+                LOGGER.debug((box, score))
+                if score <= 0.0:
+                    break
+                coords = (
+                    (box[1] * image_array.shape[1],
+                     box[0] * image_array.shape[0]),
+                    (box[3] * image_array.shape[1],
+                     box[2] * image_array.shape[0]))
+                LOGGER.debug((box, coords, image_array.shape))
+                image_draw.rectangle(coords, outline='RED')
+            del image_draw
+            image.save('bb.png')
 
 
 if __name__ == '__main__':
