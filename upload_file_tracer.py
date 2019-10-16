07d85a2614b580d0d9dfcba644cbd51ed5cddd03
Richard Sharp <richpsharp@gmail.com>
2019-10-16 15:19:39 -0700

    changing so status is updated and updating tracer code to download result

--- upload_file_tracer.py
+++ upload_file_tracer.py
@@ -1,11 +1,15 @@
 """Tracer to upload file."""
+import sys
+import shutil
+
+import time
 import numpy
 import PIL.Image
 from osgeo import gdal
 import requests
 
 
-file_path = r"D:\dams_debug\2377-1710.tif"
+file_path = sys.argv[1]
 
 png_driver = gdal.GetDriverByName('PNG')
 base_image = gdal.OpenEx(file_path, gdal.OF_RASTER)
@@ -27,4 +31,19 @@ upload_url = r.json()['upload_url']
 
 files = {'file': open(png_path, 'rb')}
 r = requests.put(upload_url, files=files)
-print(r)
+print(r.json())
+status_url = r.json()['status_url']
+
+while True:
+    r = requests.get(status_url).json()
+    print(r['status'])
+    if r['status'] != 'complete':
+        time.sleep(3)
+    else:
+        break
+print(r['bounding_box_list'])
+local_filename = r['annotated_png_url'].split('/')[-1]
+with requests.get(r['annotated_png_url'], stream=True) as r:
+    with open(local_filename, 'wb') as f:
+        shutil.copyfileobj(r.raw, f)
+print('done!')
