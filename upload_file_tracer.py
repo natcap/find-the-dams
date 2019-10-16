3d9ff70c463812cf7414f1ce6e4806ad48eb43ce
Rich Sharp <richpsharp@gmail.com>
2019-10-16 13:38:05 -0700

    tweaks for tracer code

--- upload_file_tracer.py
+++ upload_file_tracer.py
@@ -1,11 +1,23 @@
 """Tracer to upload file."""
-import sys
-
+import numpy
+import PIL.Image
+from osgeo import gdal
 import requests
 
 
-file_path = sys.argv[1]
-target_url = sys.argv[2]
+file_path = r"D:\dams_debug\2377-1710.tif"
+
+png_driver = gdal.GetDriverByName('PNG')
+base_image = gdal.OpenEx(file_path, gdal.OF_RASTER)
+png_path = 'image.png'
+png_driver.CreateCopy(png_path, base_image)
+
+image = PIL.Image.open(png_path).convert("RGB")
+height, width = image.size
+image_array = numpy.array(image.getdata()).reshape((height, width, 3))
+print(image_array.shape)
+
+target_url = "http://localhost:8080/api/v1/detect_dam"
 
 print('uploading %s to %s' % (file_path, target_url))
 
@@ -13,6 +25,6 @@ r = requests.post(target_url)
 print(r.json())
 upload_url = r.json()['upload_url']
 
-files = {'file': open(file_path, 'rb')}
+files = {'file': open(png_path, 'rb')}
 r = requests.put(upload_url, files=files)
 print(r)
