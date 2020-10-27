f393f5f54a222054cda9dde336ed77b1a29c4b74
Rich Sharp <richpsharp@gmail.com>
2020-10-26 21:54:29 -0700

    doing quad clipping in parallel with inference

--- docker-image/tensorflow_inference_server.py
+++ docker-image/tensorflow_inference_server.py
@@ -10,6 +10,7 @@ Run like this:
 import argparse
 import logging
 import os
+import queue
 import shutil
 import sys
 import threading
@@ -43,12 +44,12 @@ for dirname in [WORKSPACE_DIR, ANNOTATED_IMAGE_DIR]:
         pass
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format=(
         '%(asctime)s (%(relativeCreated)d) %(levelname)s %(name)s'
         ' [%(funcName)s:%(lineno)d] %(message)s'),
     stream=sys.stdout)
-logging.getLogger('taskgraph').setLevel(logging.DEBUG)
+logging.getLogger('taskgraph').setLevel(logging.INFO)
 LOGGER = logging.getLogger(__name__)
 
 APP = Flask(__name__, static_url_path='', static_folder='')
@@ -143,7 +144,21 @@ def make_quad_png(
         raise
 
 
-def do_inference_worker(model):
+def quad_processor(quad_offset_queue, quad_file_path_queue):
+    while True:
+        payload = quad_offset_queue.get()
+        if payload == 'STOP':
+            quad_file_path_queue.put('STOP')
+            continue
+        (quad_png_path, quad_raster_path,
+         xoff, yoff, win_xsize, win_ysize) = payload
+        make_quad_png(
+            quad_raster_path, quad_png_path,
+            xoff, yoff, win_xsize, win_ysize)
+
+
+
+def do_inference_worker(model, quad_offset_queue, quad_file_path_queue):
     """Calculate inference on the next available URL."""
     wgs84_srs = osr.SpatialReference()
     wgs84_srs.ImportFromEPSG(4326)
@@ -168,6 +183,7 @@ def do_inference_worker(model):
         quad_id = os.path.basename(os.path.splitext(quad_raster_path)[0])
         quad_slice_index = 0
         non_max_supression_box_list = []
+
         for xoff in range(0, n_cols, TRAINING_IMAGE_DIMS[0]):
             win_xsize = TRAINING_IMAGE_DIMS[0]
             if xoff + win_xsize >= n_cols:
@@ -176,61 +192,62 @@ def do_inference_worker(model):
                 win_ysize = TRAINING_IMAGE_DIMS[1]
                 if yoff + win_ysize >= n_rows:
                     yoff = n_rows-win_ysize-1
-                try:
                     quad_png_path = os.path.join(
                         WORKSPACE_DIR, '%s_%d.png' % (
                             quad_id, quad_slice_index))
                     quad_slice_index += 1
-                    make_quad_png(
-                        quad_raster_path, quad_png_path,
-                        xoff, yoff, win_xsize, win_ysize)
+                    quad_offset_queue.put(
+                        (quad_png_path, quad_raster_path,
+                         xoff, yoff, win_xsize, win_ysize))
 
-                    raw_image = numpy.asarray(PIL.Image.open(
-                        quad_png_path).convert('RGB'))[:, :, ::-1].copy()
-                    image = (
-                        raw_image.astype(numpy.float32) - [
-                            103.939, 116.779, 123.68])
-                    scale = compute_resize_scale(
-                        image.shape, min_side=800, max_side=1333)
-                    image = cv2.resize(image, None, fx=scale, fy=scale)
-                    if keras.backend.image_data_format() == 'channels_first':
-                        image = image.transpose((2, 0, 1))
+            while True:
+                payload = quad_file_path_queue.get()
+                if payload == 'STOP':
+                    break
+                quad_png_path = payload
+                raw_image = numpy.asarray(PIL.Image.open(
+                    quad_png_path).convert('RGB'))[:, :, ::-1].copy()
+                image = (
+                    raw_image.astype(numpy.float32) - [
+                        103.939, 116.779, 123.68])
+                scale = compute_resize_scale(
+                    image.shape, min_side=800, max_side=1333)
+                image = cv2.resize(image, None, fx=scale, fy=scale)
+                if keras.backend.image_data_format() == 'channels_first':
+                    image = image.transpose((2, 0, 1))
 
-                    result = model.predict_on_batch(
-                        numpy.expand_dims(image, axis=0))
-                    os.remove(quad_png_path)
-                    # correct boxes for image scale
-                    boxes, scores, labels = result
-                    boxes /= scale
+                result = model.predict_on_batch(
+                    numpy.expand_dims(image, axis=0))
+                os.remove(quad_png_path)
+                # correct boxes for image scale
+                boxes, scores, labels = result
+                boxes /= scale
 
-                    # convert box to a list from a numpy array and score to a value
-                    # from a single element array
-                    box_score_tuple_list = [
-                        (list(box), score) for box, score in zip(
-                            boxes[0], scores[0]) if score > 0.3]
-                    local_box_list = []
-                    while box_score_tuple_list:
-                        box, score = box_score_tuple_list.pop()
-                        shapely_box = shapely.geometry.box(*box)
-                        keep = True
-                        # this list makes a copy
-                        for test_box, test_score in list(box_score_tuple_list):
-                            shapely_test_box = shapely.geometry.box(*test_box)
-                            if shapely_test_box.intersects(shapely_box):
-                                if test_score > score:
-                                    # keep the new one
-                                    keep = False
-                                    break
-                        if keep:
-                            local_box_list.append([
-                                box[0]+xoff, box[1]+yoff,
-                                box[2]+xoff, box[3]+yoff])
-                    non_max_supression_box_list.extend(local_box_list)
+                # convert box to a list from a numpy array and score to a value
+                # from a single element array
+                box_score_tuple_list = [
+                    (list(box), score) for box, score in zip(
+                        boxes[0], scores[0]) if score > 0.3]
+                local_box_list = []
+                while box_score_tuple_list:
+                    box, score = box_score_tuple_list.pop()
+                    shapely_box = shapely.geometry.box(*box)
+                    keep = True
+                    # this list makes a copy
+                    for test_box, test_score in list(box_score_tuple_list):
+                        shapely_test_box = shapely.geometry.box(*test_box)
+                        if shapely_test_box.intersects(shapely_box):
+                            if test_score > score:
+                                # keep the new one
+                                keep = False
+                                break
+                    if keep:
+                        local_box_list.append([
+                            box[0]+xoff, box[1]+yoff,
+                            box[2]+xoff, box[3]+yoff])
+                non_max_supression_box_list.extend(local_box_list)
 
-                except Exception as e:
-                    LOGGER.exception('error on processing image')
-                    return str(e), 500
-        quad_png_path = '%s.png' % os.path.splitext(quad_raster_path)[0]
+        #quad_png_path = '%s.png' % os.path.splitext(quad_raster_path)[0]
         # make_quad_png(
         #     quad_raster_path, quad_png_path, 0, 0, None, None)
         # render_bounding_boxes(non_max_supression_box_list, quad_png_path)
@@ -316,9 +333,18 @@ if __name__ == '__main__':
     model = models.load_model(
         args.tensorflow_model_path, backbone_name='resnet50')
 
+    quad_offset_queue = queue.Queue()
+    quad_file_path_queue = queue.Queue()
+
+    quad_processor_worker_thread = threading.Thread(
+        target=quad_processor,
+        args=(quad_offset_queue, quad_file_path_queue))
+    quad_processor_worker_thread.daemon = True
+    quad_processor_worker_thread.start()
+
     do_inference_worker_thread = threading.Thread(
         target=do_inference_worker,
-        args=(model,))
+        args=(model, quad_offset_queue, quad_file_path_queue))
     do_inference_worker_thread.daemon = True
     do_inference_worker_thread.start()
     APP.run(host='0.0.0.0', port=args.app_port)
